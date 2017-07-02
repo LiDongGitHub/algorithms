bd4b37ce6125d2bc5e277508d4b9f8fddec335dc
LiDongGitHub <81912416@qq.com>
2017-07-02 22:10:57 +0800

    revised

--- batchGradientDescent.py
+++ batchGradientDescent.py
@@ -1,47 +1,53 @@
 # coding: utf-8
-# xionglidon
+# xionglidong
 # ubuntu16
 # jupyter
-# python=3.5
+# python3.5
 # 2017-6-28
+
 
 import numpy as np
 from numpy import genfromtxt
 
-datapath=r"/home/xiong/gradientdesent.csv" #数据地址
-dataset=genfromtxt(datapath,delimiter=',') #导入数据
 
-def getData(dataset): #得到数据
+def getData(dataset): #取得数据
     m,n=np.shape(dataset) #m,n分别是导入数据行和列
-    traindata=np.ones((m,n)) #建立一个m行，n列全为1的矩阵
-    traindata[:,:-1]=dataset[:,:-1] #切片，去掉原来数据最后一行，赋值给traindata
+    traindata=np.ones((m,n-1)) #建立一个m行，n-1列全为1的矩阵
+    traindata[:,:]=dataset[:,:-1] #切片，去掉原来数据最后一行，赋值给traindata 
     trainlabel=dataset[:,-1] #dataset最后一列赋值给tainlabel
-    return traindata,trainlabel
+    return traindata,trainlabel #返回所需数据
 
-def batchGradientDescent(x,y,theta,alpha,m,maxIterations): #批处理梯度算法
-    xTrains=x.transpose() #矩阵转置 
-    for i in range(0,maxIterations):
-        hypothesis=np.dot(x,theta)
-        loss=hypothesis-y
-        gradient=np.dot(xTrains,loss)/m
-        theta=theta-alpha*gradient 
+
+def batchGradientDescent(x,y,theta,alpha,m,maxIteration): #批处理梯度下降算法
+    xTrains=x.transpose() #转置
+    for i in range(0,maxIteration): #循环迭代maxIteration
+        hypothesis=np.dot(x,theta) #假设函数
+        loss=hypothesis-y #损失函数
+        gradient=np.dot(xTrains,loss)/m #对损失函数求梯度
+        theta=theta-alpha*gradient #得到theta
+        
+    print("theta={}".format(theta))        
     return theta
 
-def predict(x,theta):
+
+def predict(x,theta): #对得到的theta值进行测验
     m,n=np.shape(x)
-    xtest=np.ones((m,n+1))
-    xtest[:,:-1]=x
+    xtest=np.ones((m,n))
+    xtest[:,:]=x
     ypre=np.dot(xtest,theta)
     return ypre
 
-traindata,trainlabel=getData(dataset)
-m,n=np.shape(traindata)
-theta=np.ones(n)
-alpha=0.01
-maxIteration=500000
+
+datapath=r"/home/xiong/gradientdesent.csv" #数据路径
+dataset=genfromtxt(datapath,delimiter=',') #导入
+traindata,trainlabel=getData(dataset) 
+m,n=np.shape(dataset)
+alpha=0.005 #更新速率
+maxIteration=300 #迭代次数
+theta=np.array([2,5]).astype(np.float64) #权重theta值
 theta=batchGradientDescent(traindata,trainlabel,theta,alpha,m,maxIteration)
-x=np.array([[3.1,5.5],[3.3,5.9],[3.5,6.3],[3.7,6.7],[3.9,7.1]])
-print(predict(x,theta))
-    
+x=np.array([[1.05,4],[2.1,5],[5,1],[4,2]]).astype(np.float64) #测验数据集
+print(predict(x,theta)) #得出结果
+
 
 
